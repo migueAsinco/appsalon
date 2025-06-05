10ef844350f619993ea4528eac4a11cefa4748fb
migueAsinco <124771270+migueAsinco@users.noreply.github.com>
2025-06-05 12:01:57 +0200

    Update

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -19,15 +19,15 @@ class AdminController {
         }
 
         // Consultar la BD
-        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
-        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
+        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios_appsalon.nombre, ' ', usuarios_appsalon.apellido) as cliente, ";
+        $consulta .= " usuarios_appsalon.email, usuarios_appsalon.telefono, servicios.nombre as servicio, servicios.precio  ";
         $consulta .= " FROM citas  ";
-        $consulta .= " LEFT OUTER JOIN usuarios ";
-        $consulta .= " ON citas.usuarioId=usuarios.id  ";
-        $consulta .= " LEFT OUTER JOIN citasServicios ";
-        $consulta .= " ON citasServicios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN usuarios_appsalon ";
+        $consulta .= " ON citas.usuarioId=usuarios_appsalon.id  ";
+        $consulta .= " LEFT OUTER JOIN citasservicios ";
+        $consulta .= " ON citasservicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
-        $consulta .= " ON servicios.id=citasServicios.servicioId ";
+        $consulta .= " ON servicios.id= citasservicios.servicioId ";
         $consulta .= " WHERE fecha =  '${fecha}' ";
 
         $citas = AdminCita::SQL($consulta);
